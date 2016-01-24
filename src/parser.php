0d3fddc6a030513894651b2d36c0629ae303faa6
Yami Odymel <yamiodymel@gmail.com>
2016-01-24 11:08:47 +0000

    Added condition analyzer, nthToken().

--- src/parser.php
+++ src/parser.php
@@ -1,17 +1,48 @@
 <?php
 class AvaneParser extends Avane
 {
-    protected static $analyzers = ['AvaneVariableAnalyzer'];
+    protected static $analyzers = ['AvaneVariableAnalyzer', 'AvaneConditionAnalyzer'];
     
     static function parse($tokenGroup)
     {
+        $combinedToken = $tokenGroup['combinedToken'];
+        
         foreach(static::$analyzers as $analyzer)
         {
-           
-            $validated = $analyzer::validate($tokenGroup['combinedToken']);
+            $groupType = $analyzer::validate($combinedToken);
             
-            //$type = $analyzer::validate($tokenGroup);
+            if(!$groupType) break;
+            
+            $tokenGroup['type'] = $groupType;
+            
+            $analyzer::parse($tokenGroup);
+
+            
         }
+    }
+    
+    
+    static function firstToken($tokens, $targetToken)
+    {
+        return self::nthToken($tokens, 1, $targetToken);
+    }
+    
+    static function nthToken($tokens, $nth, $targetToken)
+    {
+        $count = 1;
+        
+        foreach($tokens as $token)
+        {
+            if($token['token'] == $targetToken)
+            {
+                if($count == $nth)
+                    return $token;
+                
+                $count++;
+            }
+        }
+        
+        return false;
     }
 
 }
