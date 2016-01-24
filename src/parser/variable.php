ccc327fe1eed835729502284d41ab2fad61ba66a
Yami Odymel <yamiodymel@gmail.com>
2016-01-24 11:09:37 +0000

    Can now parse the variables which with the directive.

--- src/parser/variable.php
+++ src/parser/variable.php
@@ -4,16 +4,45 @@
 class AvaneVariableAnalyzer extends Avane
 {
     protected static $validTypes = ['T_OPEN_TAG T_IDENTIFIER T_CLOSE_TAG'                        => 'T_VARIABLE',
-                                    'T_OPEN_TAG T_IDENTIFIER T_BETWEEN T_IDENTIFIER T_CLOSE_TAG' => 'T_VARIABLE'];
+                                    'T_OPEN_TAG T_IDENTIFIER T_BETWEEN T_IDENTIFIER T_CLOSE_TAG' => 'T_VARIABLE_DIRECTIVE'];
     
 	function validate($combinedToken)
 	{
 	    foreach(self::$validTypes as $type => $tokenType)
 	    {
 	        if($type == $combinedToken)
-	            exit($combinedToken);
+	            return $tokenType;
 	    }
 	    
+	    return false;
+	    
+	}
+	
+	static function parse($tokenGroup)
+	{
+	    switch($tokenGroup['type'])
+	    {
+	        case 'T_VARIABLE_DIRECTIVE':
+	            self::T_VARIABLE_DIRECTIVE($tokenGroup);
+	            break;
+	    }
+	   
+	   
+	    
+	}
+	
+	static function T_VARIABLE_DIRECTIVE($tokenGroup)
+	{
+	    //T_OPEN_TAG T_IDENTIFIER T_BETWEEN T_IDENTIFIER T_CLOSE_TAG
+	    
+	    $variableName  = AvaneParser::firstToken($tokenGroup['tokens'], 'T_IDENTIFIER');
+	    $directiveName = AvaneParser::nthToken($tokenGroup['tokens'], 2, 'T_IDENTIFIER');
+	    
+	    
+	    //<?= $this->
+	    
+	    exit(var_dump(['T_VARIABLE_DIRECTIVE' => ['variable' => $variableName,
+	                                              'directive' => $directiveName]]));
 	}
 };
 
