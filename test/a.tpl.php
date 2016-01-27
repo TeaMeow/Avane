8c62f962f02451be12f5015b2d5ac3d39bdae5f9
Yami Odymel <yamiodymel@gmail.com>
2016-01-27 07:21:45 +0000

    Just for test, nothing else.

--- test/a.tpl.php
+++ test/a.tpl.php
@@ -3,10 +3,25 @@
     <title>Caris</title>
 </head>
 <body>
-    {% if a + $b == 1 %}
+    {% if a.b.c.d + e.f + c == 1 %}
         <div>
-            { hello }
+            {% foreach users as user %}
+                
+                {% foreach user.friends as friend %}
+                    
+                    { friend.name }
+
+                {% /foreach %}
+                
+            {% /foreach %}
+            { aaa.b.c.dd }
             { world | upper }
+        </div>
+    {% /if %}
+    
+    {% if a.b.c.d + e.f == 1 %}
+        <div>
+            { cyka ? 'yes' : no }
         </div>
     {% /if %}
 </body>
--- test/test.php
+++ test/test.php
@@ -1,10 +1,11 @@
 <?php
-
+ini_set('xdebug.var_display_max_depth', '10'); 
 function e($shit)
 {
     exit(var_dump($shit));
 }
 
+include '../src/lexer.php';
 include '../src/avane.php';
 include '../src/directives.php';
 include '../src/parser.php';
