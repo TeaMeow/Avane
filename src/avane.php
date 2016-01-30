0fa42caf2527f786c1923458a73191dd6e3d055f
Yami Odymel <yamiodymel@gmail.com>
2016-01-30 16:05:13 +0000

    Added: groupSet(), capture(), endCapture(), returnPJAX(); Worked: header(), footer(); Fixed: fetch().

--- src/avane.php
+++ src/avane.php
@@ -14,7 +14,7 @@ class Avane
     /**
      * Cache Bucket
      * 
-     * When user wants to load the same template, we have no needed to parse it again,
+     * We don't have to compile the same template again,
      * we can store the path of the compiled template into the bucket, and require the template directly.
      * 
      * @var array
@@ -62,7 +62,27 @@ class Avane
     
     protected $loop = [];
     
+    /**
+     * Title
+     * 
+     * The title of the page.
+     * 
+     * @var string
+     */
+     
+    protected $title = '';
     
+    /**
+     * Content Buffer
+     * 
+     * Used to stored the rendered content which without the header and the footer.
+     * 
+     * @var string
+     */
+    
+    protected $contentBuffer = '';
+    
+    protected $isPJAX = false;
     
     
     /**
@@ -80,6 +100,7 @@ class Avane
         else
         {
             $this->categoriesPath = $thisOne;
+            $this->isPJAX         = array_key_exists($this->pjaxHeader, $_SERVER) ? $_SERVER[$this->pjaxHeader] : false;
             $this->setCategory();
         }
     }
@@ -112,14 +133,14 @@ class Avane
     {
         $this->categoryName       = $categoryName;
         $this->categoriesPath     = rtrim($this->categoriesPath, '/') . '/';
-        $this->categoryPath       = $this->categoriesPath . $categoryName . '/';
-        $this->compiledPath       = $this->categoryPath   . 'compiled/';
-        $this->scriptsPath        = $this->scriptPath     . 'scripts/';
-        $this->stylesPath         = $this->categoryPath   . 'styles/';
-        $this->templateFolderPath = $this->categoryPath   . 'tpls/';
-        $this->headerPath         = $this->categoryPath   . 'header.php';
-        $this->footerPath         = $this->categoryPath   . 'footer.php';
-        $this->variablesPath      = $this->categoryPath   . 'variables.php';
+        $this->categoryPath       = $this->categoriesPath     . $categoryName . '/';
+        $this->compiledPath       = $this->categoryPath       . 'compiled/';
+        $this->scriptsPath        = $this->scriptPath         . 'scripts/';
+        $this->stylesPath         = $this->categoryPath       . 'styles/';
+        $this->templateFolderPath = $this->categoryPath       . 'tpls/';
+        $this->headerPath         = $this->templateFolderPath . 'header' . $this->templateExtension;
+        $this->footerPath         = $this->templateFolderPath . 'footer' . $this->templateExtension;
+        $this->configPath         = $this->categoryPath       . 'config.php';
         $this->templateExtension  = '.tpl.php';
         
         return $this;
@@ -149,7 +170,6 @@ class Avane
             $this->setBucket($templateName, $this->templateCompile($templatePath));
         }
         
-        
         /** Require the template from the cache bucket, so we don't need to compile it to get the path again */
         require($this->cacheBucket[$templateName]['path']);
         
@@ -178,39 +198,7 @@ class Avane
     }
     
     
-    
-    
-    /**
-     * Is PJAX
-     * 
-     * Check the header to see if there's any PJAX request header inside of it.
-     * 
-     * @return bool
-     */
 
-    function isPJAX()
-    {
-        return (array_key_exists($this->pjaxHeader, $_SERVER) && $_SERVER[$this->pjaxHeader] === 'true');
-    }
-
-    
-    
-    
-    /**
-     * Render
-     * 
-     * Use ob_* functions and get the rendered template content.
-     * 
-     * @return string
-     */
-     
-    function render($templateName)
-    {
-        
-    }
-    
-    
-    
     
     /**
      * Fetch
@@ -225,30 +213,84 @@ class Avane
     
     function fetch($templateName, $variables = null)
     {
-        /** Compile the template If the cache bucket doesn't have the template */
-        if(!isset($this->cacheBucket[$templateName]))
-        {
-            $templatePath = $this->getTemplatePath($templateName);
-            
-            $this->setBucket($templateName, $this->templateCompile($templatePath));
-        }
-        
-        
-        return $this->cacheBucket[$templateName]['content'];
-        
-        return $this;
+        $this->capture();
+        $this->load($templateName, $variables);
+        $this->endCapture();
     }
         
-        
-    function header()
+    
+    
+    
+    /**
+     * Header
+     * 
+     * The header of the page, and we do something here also.
+     * 
+     * @param 
+     */
+     
+    function header($title = '', $variables = null)
     {
+        /** Set the title */
+        $this->title = $title;
+        
+        /** And set the title as the avane variable */
+        $this->set('title', $title);
+        
+        /** Load the configs of the category */
+        @include_once($this->configPath);
+        
+        /** Set variables **/
+        $this->groupSet($variables);
+        
+        /** Set the json header if it's a PJAX request, otherwise load the header template */
+        if($this->isPJAX())
+            header('Content-Type: application/json; charset=utf-8');
+
+        /** Capture the rendered content from now on */
+        $this->capture()
+             /** Load the header and require it */
+             ->load('header')
+             /** Stop capturing, and store the captured content to the output buffer array */
+             ->endCapture('header')
+             /** Start another capture action for the content part */
+             ->capture();
+        
         return $this;
     }
     
+    
+    
+    
+    /**
+     * Footer
+     *
+     * @return Avane
+     */
+     
     function footer()
     {
+        /** Stop capturing, what we got here is a content-only part, store it either */
+        $this->endCapture('content')
+             /** Now capture the footer part */
+             ->capture()
+             /** Require the footer template */
+             ->load('footer')
+             /** And stop capturing, you know what's next right? */
+             ->endCapture('footer');
+        
+        /** Return the rendered content if it's a PJAX request */
+        if($this->isPJAX)
+            echo json_encode($this->returnPJAX());
+
+        //flush();
+        
         return $this;
     }
+    
+    
+    
+    
     
     
     
@@ -302,6 +344,111 @@ class Avane
         
         return ['path'    => $info['path'],
                 'content' => $info['content']];
+    }
+    
+    
+    
+    
+    /***********************************************
+    /***********************************************
+    /****************** P J A X ********************
+    /***********************************************
+    /***********************************************
+    
+    /**
+     * Return PJAX
+     * 
+     * Combine the different informations based on the PJAX header content.
+     * 
+     * @return array   The datas.
+     */
+     
+    function returnPJAX()
+    {
+        if(!$this->isPJAX)
+            return false;
+            
+        $types = explode(', ', $this->isPJAX);
+        $data  = [];
+        
+        if(in_array('title', $types))
+            $data['title'] = $this->title;
+        
+        if(in_array('html', $types))
+            $data['html'] = $this->outputBuffer['header']  . 
+                            $this->outputBuffer['content'] . 
+                            $this->outputBuffer['footer'];
+        
+        if(in_array('header', $types))
+            $data['header'] = $this->outputBuffer['header'];
+        
+        if(in_array('content', $types))
+            $data['content'] = $this->outputBuffer['content'];
+        
+        if(in_array('footer', $types))
+            $data['footer'] = $this->outputBuffer['footer'];
+        
+        return $data;
+    }
+    
+    
+    
+    
+    /***********************************************
+    /***********************************************
+    /**************** C A P T U R E ****************
+    /***********************************************
+    /***********************************************
+    
+    /**
+     * Capture
+     * 
+     * Capture the rendered content after this function.
+     * 
+     * @return Avane
+     */
+     
+    function capture()
+    {
+        if(!$this->isPJAX)
+            return $this;
+            
+        ob_start();
+        
+        return $this;
+    }
+    
+    
+    
+    
+    /**
+     * End Capture
+     * 
+     * Stop capturing, and store the captured content to the right array.
+     */
+    
+    function endCapture($position = null)
+    {
+        if(!$this->isPJAX && $position)
+            return this;
+        
+        switch($position)
+        {
+            case 'header':
+                $this->outputBuffer['header']  = ob_get_clean();
+                break;
+            case 'content':
+                $this->outputBuffer['content'] = ob_get_clean();
+                break;
+            case 'footer':
+                $this->outputBuffer['footer']  = ob_get_clean();
+                break;
+            case null:
+                return ob_get_clean();
+                break;
+        }
+        
+        return $this;
     }
     
     
@@ -429,6 +576,28 @@ class Avane
     function set($key, $value)
     {
         $this->vault[$key] = $value;
+        
+        return $this;
+    }
+    
+    
+    
+    
+    /**
+     * Group Set
+     * 
+     * Same as the set() just changed to an array this time.
+     * 
+     * @param array $vars   The variables, key as the name, value as the value of the variable.
+     * 
+     * @return Avane
+     */
+    
+    function groupSet($vars = null)
+    {
+        if(!empty($vars))
+            foreach($vars as $name => $value)
+                $this->vault[$name] = $value;
         
         return $this;
     }
