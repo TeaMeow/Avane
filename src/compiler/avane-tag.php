039576563681ce5c79f33742787218731b21f941
Yami Odymel <yamiodymel@gmail.com>
2016-02-19 11:15:23 +0000

    Separate the avane tags codes.

---
+++ src/compiler/avane-tag.php
@@ -0,0 +1,74 @@
+<?php
+class AvaneAvTagCompiler extends Avane
+{
+    protected $avaneNames;
+    protected $content;
+    
+    function __construct($thisOne)
+    {
+        if($thisOne) parent::__construct($thisOne);
+        
+        if(!$this->anaveNames)
+            $this->avaneNames = file_exists($this->avNamesPath) ? json_decode(file_get_contents($this->avNamesPath), true)
+                                                                : [];
+    }
+    
+    
+    function compile($htmlContent)
+    {
+        $this->content = $htmlContent;
+        
+        $this->collect()
+             ->outputJs();
+    }
+    
+    function collect()
+    {
+        $content = str_get_html($this->content);
+
+
+
+        foreach($content->find('*[av-group]') as $element)
+        {
+            $group = $element->attr['av-group'];
+            
+            foreach($element->find('*[av-name]') as $child)
+            {
+                if(!isset($this->avaneNames[$group]))
+                    $this->avaneNames[$group] = [];
+
+                array_push($this->avaneNames[$group], $child->attr['av-name']);
+                $this->avaneNames[$group] = array_unique($this->avaneNames[$group]);
+            }
+        }
+       
+        file_put_contents($this->avNamesPath, json_encode($this->avaneNames));
+        
+        return $this;
+    }
+    
+    
+    
+    
+    function outputJs()
+    {
+        $js = '';
+    
+        foreach($this->avaneNames as $group => $nameList)
+        {
+            foreach($nameList as $name)
+            {
+                $js .= "var \${$group}_$name = $('[av-group=\"$group\"] [av-name=\"$name\"]'); \n";
+                $js .= "var {$group}_$name = \"[av-group='$group'] [av-name='$name']\" \n";
+            }
+        }
+        
+        file_put_contents($this->avScriptPath, $js);
+    }
+    
+    function outputCss()
+    {
+        
+    }
+} 
+?>
--- src/compiler/template.php
+++ src/compiler/template.php
@@ -27,8 +27,9 @@ class AvaneTemplateCompiler extends Avane
   
         $compiledContent = $this->parser->parse($templateContent);
         
+        $this->avaneTagCompiler = new AvaneAvTagCompiler($this);
         
-        $this->collectAvaneTags($compiledContent);
+        $this->avaneTagCompiler->compile($compiledContent);
         
         
         file_put_contents($this->templateCachedPath, $compiledContent);
@@ -37,56 +38,7 @@ class AvaneTemplateCompiler extends Avane
                 'content' => $compiledContent];
     }
     
-    
-    
-    function collectAvaneTags($compiledContent)
-    {
-        if(file_exists($this->avNamesPath))
-            $names = json_decode(file_get_contents($this->avNamesPath), true);
-        else
-            $names = [];
-        
-        $a = str_get_html($compiledContent);
-        
-        foreach($a->find('*[av-name]') as $element)
-        {
-            
-            
-            $avGroup = $element->attr['av-group'] ? $element->attr['av-group']
-                                                  : '%';
-            
-            if(!isset($names[$avGroup]))
-                $names[$avGroup] = [];
-             
-             
-              
-            array_push($names[$avGroup], $element->attr['av-name']);
-        }
-        
-        file_put_contents($this->avNamesPath, json_encode($names));
-        
-        $this->jsAvaneTags($names);
-    }
-    
-    
-    
-    function jsAvaneTags($names)
-    {
-        $js = '';
-        
-        foreach($names as $group => $nameList)
-        {
-            foreach($nameList as $name)
-            {//var $$group_$name = $('[av-name=\"$name\"][av-group=\"$group\"]');
-                $js .= 'var $' . $group .'_' . $name . ' = $(\'[av-name="' . $name .'"][av-group="' . $group . '"]\');' . "\n";
-            }
-        }
-        
-        file_put_contents($this->avScriptPath, $js);
-    }
-    
-    
-    
+
     
     
     
