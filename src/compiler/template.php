534c1afbf58fb120641c8ef2f6d89dd875c4a1a2
Yami Odymel <yamiodymel@gmail.com>
2016-02-19 09:45:36 +0000

    Can now collect avane tags (still testing), and forceCompile.

--- src/compiler/template.php
+++ src/compiler/template.php
@@ -17,19 +17,79 @@ class AvaneTemplateCompiler extends Avane
         $this->templateMD5        = md5_file($templatePath);
         $this->templateCachedPath = $this->cachedPath();
         
-        if($this->hasCached())
+        if($this->hasCached() && !$this->forceCompile)
             return ['path'    => $this->templateCachedPath,
                     'content' => file_get_contents($this->templateCachedPath)];
             
         
         $templateContent = file_get_contents($templatePath);
+        
+  
         $compiledContent = $this->parser->parse($templateContent);
+        
+        
+        $this->collectAvaneTags($compiledContent);
+        
         
         file_put_contents($this->templateCachedPath, $compiledContent);
         
         return ['path'    => $this->templateCachedPath,
                 'content' => $compiledContent];
     }
+    
+    
+    
+    function collectAvaneTags($compiledContent)
+    {
+        if(file_exists($this->avNamesPath))
+            $names = json_decode(file_get_contents($this->avNamesPath), true);
+        else
+            $names = [];
+        
+        $a = str_get_html($compiledContent);
+        
+        foreach($a->find('*[av-name]') as $element)
+        {
+            
+            
+            $avGroup = $element->attr['av-group'] ? $element->attr['av-group']
+                                                  : '%';
+            
+            if(!isset($names[$avGroup]))
+                $names[$avGroup] = [];
+             
+             
+              
+            array_push($names[$avGroup], $element->attr['av-name']);
+        }
+        
+        file_put_contents($this->avNamesPath, json_encode($names));
+        
+        $this->jsAvaneTags($names);
+    }
+    
+    
+    
+    function jsAvaneTags($names)
+    {
+        $js = '';
+        
+        foreach($names as $group => $nameList)
+        {
+            foreach($nameList as $name)
+            {//var $$group_$name = $('[av-name=\"$name\"][av-group=\"$group\"]');
+                $js .= 'var $' . $group .'_' . $name . ' = $(\'[av-name="' . $name .'"][av-group="' . $group . '"]\');' . "\n";
+            }
+        }
+        
+        file_put_contents($this->avScriptPath, $js);
+    }
+    
+    
+    
+    
+    
+    
     
     function hasCached()
     {
