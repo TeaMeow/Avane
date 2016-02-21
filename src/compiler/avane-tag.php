1702382a4803039305ac74c8512fcb0735b350c3
Yami Odymel <yamiodymel@gmail.com>
2016-02-21 17:56:31 +0000

    Added many comments.

--- src/compiler/avane-tag.php
+++ src/compiler/avane-tag.php
@@ -12,7 +12,17 @@ class AvaneAvTagCompiler extends Avane
             $this->avaneNames = file_exists($this->avNamesPath) ? json_decode(file_get_contents($this->avNamesPath), true)
                                                                 : [];
     }
-
+    
+    
+    
+    
+    /**
+     * Compile
+     * 
+     * Collects the avane tags, and compile the avane helper files.
+     * 
+     * @return AvaneAvTagCompiler
+     */
 
     function compile($htmlContent)
     {
@@ -24,23 +34,36 @@ class AvaneAvTagCompiler extends Avane
         $this->collect()
              ->outputJs();
     }
-
+    
+    
+    
+    
+    /**
+     * Collect
+     *
+     * Parses the html content, and get all the av-groups and av-names,
+     * then push them into a file.
+     * 
+     * @return AvaneAvTagCompiler
+     */
+     
     function collect()
     {
         $content = str_get_html($this->content);
 
-
-
+        /** Search for all the av-groups */
         foreach($content->find('*[av-group]') as $element)
         {
             $group = $element->attr['av-group'];
-
+            
+            /** Push all the elements which are under this group to the list */
             foreach($element->find('*[av-name]') as $child)
             {
                 $this->pushGroup($group, $child->attr['av-name']);
             }
         }
 
+        /** Includes self when av-name and av-group were in the same tag */
         /*foreach($content->find('*[av-name]') as $element)
         {
             if(!isset($element->attr['av-group']))
@@ -49,11 +72,22 @@ class AvaneAvTagCompiler extends Avane
                 $this->pushGroup($element->attr['av-group'], $element->attr['av-name']);
         }*/
 
+        /** Update the avane names file */
         file_put_contents($this->avNamesPath, json_encode($this->avaneNames));
 
         return $this;
     }
 
+    
+    
+    
+    /**
+     * Push Group
+     * 
+     * Push a name into a group.
+     * 
+     * @return AvaneAvTagCompiler
+     */
 
     function pushGroup($group, $name)
     {
@@ -67,7 +101,16 @@ class AvaneAvTagCompiler extends Avane
     }
 
 
-
+    
+    
+    /**
+     * Output JS
+     * 
+     * Converts the names to the javascript variables,
+     * so we can use it quickly in the javascript.
+     * 
+     * @return AvaneAvTagCompiler
+     */
 
     function outputJs()
     {
@@ -86,8 +129,19 @@ class AvaneAvTagCompiler extends Avane
 
 
         file_put_contents($this->avScriptPath, $js);
+        
+        return $this;
     }
 
+    
+    
+    
+    /**
+     * Output Css
+     * 
+     * Now disabled.
+     */
+     
     static function outputCss($string)
     {
         return preg_replace('/%%(.*?)%%/', '[av-group="$1"] *:not([av-group]),'."\n".'[av-group="$1"] >', $string);
