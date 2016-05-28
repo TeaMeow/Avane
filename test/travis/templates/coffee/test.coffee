0604f195e0844adc80bfe1e639e549ba3376676b
Yami Odymel <yamiodymel@gmail.com>
2016-05-28 19:01:18 +0000

    Unit testing.

---
+++ test/travis/templates/coffee/test.coffee
@@ -0,0 +1,2 @@
+test = ->
+    alert 'Ello.'
--- test/travis/templates/default/config.php
+++ test/travis/templates/default/config.php
@@ -3,8 +3,8 @@ $this->import('test', 'test.css', true)
      ->import('test', 'test.js', true)
      ->import('test', '//ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js');
 
-$this->sassSet('music', 'music.sass')
-     ->sassSet('musics', 'music.sass');
+$this->sassSet('music', 'music.sass');
+$this->coffeeSet('music', ['test.coffee']);
 
 $this->includeSet('a', 'include-tests/a');
 $this->includeSet('blockA', 'block-tests/a');
