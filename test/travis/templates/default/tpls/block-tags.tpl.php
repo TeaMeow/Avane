77f611925223736e34a3a7941f2227871188e7b3
Yami Odymel <yamiodymel@gmail.com>
2016-05-28 17:09:22 +0000

    Unit testing.

--- test/travis/templates/default/tpls/block-tags.tpl.php
+++ test/travis/templates/default/tpls/block-tags.tpl.php
@@ -1,5 +1,5 @@
 <!-- Test -->
-{% block title %}
+{% block test %}
     Yolo.
 {% /block %}
 <!-- / Test -->
@@ -11,4 +11,22 @@
 {% block hello %}
     Ello.
 {% /block %}
+<!-- / Test -->
+
+
+<!-- Test -->
+{% block header %}Header - {% /block %}
+
+{% extends header %}
+
+{% block header prepend %} Test {% /block %}
+<!-- / Test -->
+
+
+<!-- Test -->
+{% block header2 %}Header2 - {% /block %}
+
+{% extends header2 %}
+
+{% block header2 append %} Test2 {% /block %}
 <!-- / Test -->
