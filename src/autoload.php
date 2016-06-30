ab866a5c3c1a42a74232119335396aa5b7348b3f
Yami Odymel <yamiodymel@gmail.com>
2016-06-30 12:26:55 +0000

    Added __DIR__ for the paths.

--- src/autoload.php
+++ src/autoload.php
@@ -1,8 +1,8 @@
 <?php
-require 'vendor/autoload.php';
-require 'library/Yaml.php';
-require 'getallheaders.php';
-require 'main.php';
-require 'compiler/coffee.php';
-require 'compiler/sass.php';
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/library/Yaml.php';
+require __DIR__ . '/getallheaders.php';
+require __DIR__ . '/main.php';
+require __DIR__ . '/compiler/coffee.php';
+require __DIR__ . '/compiler/sass.php';
 ?>
