560facfe9415255b8b94f1dff4c3e048dabedffe
Yami Odymel <yamiodymel@gmail.com>
2016-07-05 16:17:32 +0000

    Added a test for coffee compiler error, and trying to collect the coffee compiled result.

--- test/avaneTest.php
+++ test/avaneTest.php
@@ -5,10 +5,28 @@ class AvaneTest extends \PHPUnit_Framework_TestCase
     {
         $Avane = new Avane\Main(__DIR__ . '/template');
 
-        $Avane->render('test');
+        $this->assertEquals($Avane->fetch('test'), '<div>Hello, World!</div>');
     }
 
     function testPjax()
+    {
+        $Avane = new Avane\Main(__DIR__ . '/template');
+
+        /** Simulate the PJAX header content */
+        $Avane->isPJAX = 'title, html, content, footer';
+
+        ob_start();
+
+        $Avane->header('header')
+              ->render('test')
+              ->footer('footer');
+
+        $returned = ob_get_clean();
+
+        $this->assertEquals($returned, '{"title":null,"html":"<html><head><\/head><body><div>Hello, World!<\/div><\/body><\/html>","content":"<div>Hello, World!<\/div>","footer":"<\/body><\/html>"}');
+    }
+
+    function testFullyPjax()
     {
         $Avane = new Avane\Main(__DIR__ . '/template');
 
@@ -24,28 +42,38 @@ class AvaneTest extends \PHPUnit_Framework_TestCase
     {
         $Avane = new Avane\Main(__DIR__ . '/template_coffee');
 
-        $Avane->render('test');
+        $this->assertEquals($Avane->fetch('test'), '<div>Hello, World!</div>');
+
+        echo file_get_content(__DIR__ . '/template_coffee/scripts/a.js');
+        echo file_get_content(__DIR__ . '/template_coffee/scripts/c.js')
+    }
+
+    function testCoffeeError()
+    {
+        $Avane = new Avane\Main(__DIR__ . '/template_coffeeError');
+
+        $this->assertEquals($Avane->fetch('test'), '<div>Hello, World!</div>');
     }
 
     function testRubySass()
     {
         $Avane = new Avane\Main(__DIR__ . '/template_sass');
 
-        $Avane->render('test');
+        $this->assertEquals($Avane->fetch('test'), '<div>Hello, World!</div>');
     }
 
     function testSassC()
     {
         $Avane = new Avane\Main(__DIR__ . '/template_sassc');
 
-        $Avane->render('test');
+        $this->assertEquals($Avane->fetch('test'), '<div>Hello, World!</div>');
     }
 
     function testSassTracker()
     {
         $Avane = new Avane\Main(__DIR__ . '/template_sassTracker');
 
-        $Avane->render('test');
+        $this->assertEquals($Avane->fetch('test'), '<div>Hello, World!</div>');
     }
 }
 ?>
