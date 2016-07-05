6f23f434b3dc3771fb2101c5aac4dac1a5c00996
Yami Odymel <yamiodymel@gmail.com>
2016-07-05 11:27:53 +0000

    Fixed path for unit test.

--- test/avaneTest.php
+++ test/avaneTest.php
@@ -3,14 +3,14 @@ class AvaneTest extends \PHPUnit_Framework_TestCase
 {
     function testRender()
     {
-        $Avane = new Avane\Main(__DIR__ . '/default');
+        $Avane = new Avane\Main(__DIR__ . '/template');
 
         $Avane->render('test');
     }
 
     function testPjax()
     {
-        $Avane = new Avane\Main(__DIR__ . '/default');
+        $Avane = new Avane\Main(__DIR__ . '/template');
 
         $Avane->header('header')
               ->render('test')
